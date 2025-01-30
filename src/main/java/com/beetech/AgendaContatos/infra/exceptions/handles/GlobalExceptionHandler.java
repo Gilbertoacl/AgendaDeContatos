d945d3c8b69b63cd485748dc45771df6eb2cd997
Gilbertoacl <betoacl@hotmail.com>
2025-01-30 15:39:11 -0300

    Adicionado mais verificações de e exceptions para retorno.

--- src/main/java/com/beetech/AgendaContatos/infra/exceptions/handles/GlobalExceptionHandler.java
+++ src/main/java/com/beetech/AgendaContatos/infra/exceptions/handles/GlobalExceptionHandler.java
@@ -1,23 +1,47 @@
 package com.beetech.AgendaContatos.infra.exceptions.handles;
 
 import com.beetech.AgendaContatos.infra.exceptions.usuarioExceptions.UsuarioJaExisteException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    //Excessão para usuário ja cadastrado
     @ExceptionHandler(UsuarioJaExisteException.class)
     public ResponseEntity<String> handleUsuarioJaExisteException(UsuarioJaExisteException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
-    //Excessão geral
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro inesperado: " + ex.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("ERRO: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity tratarErroBadCredentials() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciais inválidas");
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity tratarErroAuthentication() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Falha na autenticação");
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity tratarErroAcessoNegado() {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Acesso negado");
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarErro404() {
+        return ResponseEntity.notFound().build();
     }
 }
